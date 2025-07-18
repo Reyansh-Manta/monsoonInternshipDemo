585291a6e8d5f823e93c4629eb173cf62215f3fb
Reyansh-Manta <mantareyansh@gmail.com>
2025-07-18 20:28:48 +0530

    Refactor canvas drawing logic and update angle calculations for improved clarity and functionality

--- script.js
+++ script.js
@@ -25,54 +25,70 @@ ctx.setLineDash([])
 
 
 
-
-// for angle of refraction
-
-
-
-
-
-
-
 const Btn = document.getElementById("Btn");
 
 Btn.addEventListener("click", function() {
 
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+ctx.strokeStyle = `black`
+ctx.lineWidth = 2
+
+ctx.beginPath()
+ctx.moveTo(0, CentreY)
+ctx.lineTo(canvas.width, CentreY)
+ctx.stroke()
+
+ctx.setLineDash([5, 5])
+ctx.beginPath()
+ctx.moveTo(CentreX, 0)
+ctx.lineTo(CentreX, canvas.height)
+ctx.stroke()
+ctx.setLineDash([])
+
 const incidentAngel = document.getElementById("incidentAngle");
 const Ref1 = document.getElementById("mu 1");
 const Ref2 = document.getElementById("mu 2");
+    // console.log(incidentAngel.value);
+    // console.log(Ref1.value);
+    // console.log(Ref2.value);
     
     
-const MSinOfRefractedAngle = Ref1.value/Ref2.value * Math.sin(incidentAngel.value * (Math.PI / 180));
+const SinOfRefractedAngle = Ref1.value/Ref2.value * Math.sin(incidentAngel.value * (Math.PI / 180));
 
-console.log(MSinOfRefractedAngle);
+console.log(SinOfRefractedAngle);
 
-const ref = Math.asin(MSinOfRefractedAngle);
+const ref = Math.asin(SinOfRefractedAngle)*180/Math.PI;
 
 
 console.log(ref);
 
-const slopeOfIncidence = Math.tan((incidentAngel + 90) * (Math.PI / 180))
-const slopeOfRefraction = Math.tan((ref + 90) * (Math.PI / 180))
+const slopeOfIncidence = Math.tan((90 - incidentAngel.value) * (Math.PI / 180))
+console.log(slopeOfIncidence);
+const slopeOfRefraction = Math.tan((90 - ref) * (Math.PI / 180))
+console.log(slopeOfRefraction);
 
-const x = 200
+const x = 400
 
 const yi = slopeOfIncidence * x
+console.log(yi);
+
 
 const yr = slopeOfRefraction * x
+console.log(yr);
 
 
 
 ctx.strokeStyle = "cyan";
 ctx.lineWidth = 2;
 ctx.beginPath();
-ctx.moveTo(CentreX - 200, CentreY - yi);
+ctx.moveTo(CentreX - x, CentreY - yi);
 ctx.lineTo(CentreX, CentreY);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.moveTo(CentreX, CentreY);
-ctx.lineTo(CentreX + 200, CentreY + yr);
+ctx.lineTo(CentreX + x, CentreY + yr);
 ctx.stroke();
 
 function drawArrowhead(x, y, angle) {
@@ -86,10 +102,10 @@ function drawArrowhead(x, y, angle) {
     ctx.fill();
 }
 
-drawArrowhead(CentreX - 100, CentreY - 75, Math.atan2(150, 200));
+drawArrowhead(CentreX - x/2, CentreY - yi/8, Math.atan2(CentreY - yi, CentreX - x));
 
 
-drawArrowhead(CentreX + 95, CentreY + 125, Math.atan2(200, 150));
+drawArrowhead(CentreX + x/2, CentreY + yr/8, Math.atan2(CentreY + yr, CentreX + x));
 
 ctx.fillStyle = "black";
 ctx.font = "16px Arial";
@@ -99,7 +115,7 @@ ctx.fillText("i", CentreX - 13, CentreY - 20);
 ctx.fillText("r", CentreX + 10, CentreY + 45);
 
 
-// to be changed to a proper code to change on every calue of theta by implementing a python script
+// to be changed to a proper code to change on every value of theta by implementing a prpper script
 
 ctx.beginPath();
 ctx.arc(CentreX, CentreY, 40, 4.7, 10.1, true); // angle i
